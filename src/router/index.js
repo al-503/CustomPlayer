c5a1a05b8c6f1737cbe7482f3e7a0226774a3995
al-503 <axel.ridray@sfr.fr>
2022-05-13 15:40:03 +0200

    restart des premières steps du projet

--- src/router/index.js
+++ src/router/index.js
@@ -1,20 +1,20 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import PlayerView from '@/views/PlayerView.vue'
 
 const routes = [
   {
     path: '/',
-    name: 'home',
-    component: HomeView
-  },
-  {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    name: 'PlayerView',
+    component: PlayerView
   }
+  // {
+  //   // path: '/about',
+  //   // name: 'about',
+  //   // // route level code-splitting
+  //   // // this generates a separate chunk (about.[hash].js) for this route
+  //   // // which is lazy-loaded when the route is visited.
+  //   // component: () => import(/* webpackChunkName: "about" */ '@/views/AboutView.vue')
+  // }
 ]
 
 const router = createRouter({
@@ -23,3 +23,4 @@ const router = createRouter({
 })
 
 export default router
+
