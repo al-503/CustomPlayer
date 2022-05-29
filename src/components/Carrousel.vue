545b0f02d90fe884fa94243243077537b23b8406
al-503 <axel.ridray@sfr.fr>
2022-05-29 15:22:31 +0200

    thumb et placement des logos

--- src/components/Carrousel.vue
+++ src/components/Carrousel.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="carrousel">
-    <div class="carrousel-slides" >
+    <div class="carrousel-slides">
       <CarrouselSlide v-for="channel in channels"
                       :key="channel.id"
                       :logo="channel.logo"
@@ -8,14 +8,14 @@
                       :title="channel.programme[0].title"
                       :startTime="channel.programme[0].startTime"
                       :endTime="channel.programme[0].endTime" />
-    </div>
   </div>
+</div>
 </template>
 
 <script>
 import Store from "@/store"
 
-import CarrouselSlide from "@/components/CarrouselSlide.vue"
+import CarrouselSlide from "@/components/CarrouselSlides.vue"
 
 export default {
   components: {
@@ -28,5 +28,21 @@ export default {
 </script>
 
 <style lang="scss">
+.carrousel {
+  width: 50%;
+  height: 100%;
+  overflow: hidden;
+
+  background: linear-gradient(
+    270deg,
+    rgba(132, 132, 132, 0) 0%,
+    rgba(69, 69, 69, 0.5) 51.56%,
+    rgba(0, 0, 0, 0.75) 100%
+  );
+}
+
+.carrousel-slides {
+  width: 50%;
+}
 
 </style>
