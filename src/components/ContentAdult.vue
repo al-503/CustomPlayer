540389df68e013f71ab5ae7ec32ccd5aacedc81e
OlgaGiraudon <giraudon.olga@gmail.com>
2022-06-05 21:23:00 +0200

    Ajout logoHour et icone

--- src/components/ContentAdult.vue
+++ src/components/ContentAdult.vue
@@ -1,17 +1,30 @@
 <template>
     <div class="contentAdultMain">
+    <CurrentHour class="curentHourAdult" />
     <CurrentChannelLogo :logo="currentChannel.logo" id="logoChannelAdult" />
+
      <div class="contentAdultDiv">
-    <p class="contentAdultText">Contenu bloqué</p>
+    <p class="contentAdultText">Contenu bloqué</p> <br>
+    <fa class="lockIcon" icon="lock" />
   </div>
     </div >
 </template>
 <script>
 import Store from "@/store";
 import CurrentChannelLogo from "@/components/CurrentChannelLogo.vue";
+import CurrentHour from "@/components/CurrentHour.vue";
+
 export default {
+   data() {
+    return {
+      durationHour: 0,
+      durationMinute: 0,
+      ProvisDurationMinute: 0,
+    };
+  },
   components: {
     CurrentChannelLogo,
+    CurrentHour
   },
 
   computed: {
@@ -39,20 +52,29 @@ export default {
   left: 36%;
   top: 35%;
   border-radius: 25px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  padding-top: 1%;
+  padding-bottom: 1%;
 }
 .contentAdultText{
   font-family: "Roboto", "Open Sans", sans-serif;
   color: white;
   font-weight: bold;
   font-size: 48px;
-  text-align: center;
-  margin-top: 21%;
 }
-#logoChannelAdult{
+#logoChannelAdult, .lock-keyhole {
   z-index: 3;
 }
 #logoChannelAdult img{
   top: 10%;
+}
+.lockIcon{
+  height: 100px;
+  z-index: 4;
+  color: white;
 }
 
 </style>
