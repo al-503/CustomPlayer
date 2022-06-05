a6f88b07e52ef7b58f5d4a3ba7a350dc43581e38
OlgaGiraudon <giraudon.olga@gmail.com>
2022-06-05 16:48:03 +0200

    InfoButton html+css

---
+++ src/components/InfoButton.vue
@@ -0,0 +1,27 @@
+<template>
+     <input type="submit" value="INFO" class="infoButtton" v-on:keyup.esc="returnToMainPage" >
+</template>
+
+<script>
+import Store from "@/store";
+export default {
+  methods:{
+        hideInfoLightAndShowInfoMax: function () {
+          
+        }
+    }
+}
+</script>
+
+<style>
+.infoButtton {
+    width: 146px;
+    height: 47px;
+    border-radius: 15px;
+    border: 4px solid white;
+    font-size: 28px;
+    color: white;
+    font-style: italic;
+    background-color: #3f3f3f;
+}
+</style>
